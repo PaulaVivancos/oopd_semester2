31851dad846eb84c62acad36778f234f4fdea484
Stefan123 <stefan.radulescu@students.salle.url.edu>
2026-04-27 09:23:54 +0200

    added the entities

---
+++ src/Business/entities/GeneratorType.java
@@ -0,0 +1,10 @@
+package Business.entities;
+
+public class GeneratorType {
+
+    private String name;
+    private double baseCost;
+    private double baseProduction;
+    private String imagePath;
+    private double costIncrement;
+}
