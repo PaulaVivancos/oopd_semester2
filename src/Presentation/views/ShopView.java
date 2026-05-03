e750050973d43c0b412f43b2538aacdceb9fc65a
fabius-kudo <daikifabius.kudo@students.salle.url.edu>
2026-05-03 14:22:38 +0200

    started shop view

---
+++ src/Presentation/views/ShopView.java
@@ -0,0 +1,18 @@
+package Presentation.views;
+
+import javax.swing.*;
+
+public class ShopView extends BaseView {
+    private JPanel mainPanel;
+
+
+    @Override
+    protected void buildMenu(JPopupMenu menu) {
+
+    }
+
+    @Override
+    protected void initComponents() {
+        mainPanel = new JPanel();
+    }
+}
